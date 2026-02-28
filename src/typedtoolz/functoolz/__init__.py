eb1fb31348d988b7930a6aef3f5b9e15e3e6a8bb
gordoncyu <gordoncyu@gmail.com>
2026-02-28 12:14:14 -0700

    cleanup

--- src/typedtoolz/functoolz/__init__.py
+++ src/typedtoolz/functoolz/__init__.py
@@ -1,7 +1,8 @@
 from ._excepts import excepts, union_error
 from ._finalizer import finalizer
-from ._with import wlth, wlth_op
+from ._with import wlth
 from ._reduce import reduce, reduced
+from . import return_parameter
 
-__all__ = ["excepts", "union_error", "finalizer", "reduce", "reduced", "wlth", "wlth_op"]
+__all__ = ["excepts", "union_error", "finalizer", "reduce", "reduced", "wlth", "return_parameter"]
 
--- src/typedtoolz/functoolz/_with.py
+++ src/typedtoolz/functoolz/_with.py
@@ -2,7 +2,7 @@ from io import TextIOBase, UnsupportedOperation
 from typing import Callable, ContextManager, TypeVar, cast
 from typedtoolz import identityv
 from typedtoolz.functoolz.bring import bring
-from typedtoolz.functoolz.curry import Curried3, curry
+from typedtoolz.functoolz.curry import curry
 from typedtoolz.functoolz.curryv import curryv
 
 def _wlth[A, R](res: ContextManager[A], body: Callable[[A], R]) -> R:
@@ -11,53 +11,53 @@ def _wlth[A, R](res: ContextManager[A], body: Callable[[A], R]) -> R:
 
 wlth = curry(_wlth)
 
-with_op = curry(bring(1, _wlth))
-
-thing = wlth(open("asdfa"), lambda f: f.read())
-thing = wlth(open("asdfa"))(lambda f: f.read())
-
-from typing import Union
-from typedtoolz.functoolz import excepts, union_error
-A1 = TypeVar("A1")
-A2 = TypeVar("A2")
-A3 = TypeVar("A3")
-
-file_errors = (
-    FileNotFoundError,
-    PermissionError,
-    IsADirectoryError,
-    NotADirectoryError,
-    FileExistsError,
-    ValueError,
-    TypeError,
-    UnsupportedOperation,
-    LookupError,
-)
-open_errors = (
-    ValueError,
-    TypeError,
-    UnsupportedOperation,
-    BlockingIOError,
-    InterruptedError,
-    TimeoutError,
-    IsADirectoryError,
-    NotADirectoryError,
-    PermissionError,
-    FileNotFoundError,
-    ConnectionResetError,
-    ConnectionAbortedError,
-    BrokenPipeError,
-)
-
-saferead = union_error(
-        open_errors, 
-        with_op(
-            cast(
-                Callable[[TextIOBase], str],
-                lambda f: f.read()
-                )
-            )
-        )
-res = saferead(open("asdf"))
-safereadfile = union_error(file_errors, lambda path: saferead(path))
-res = safereadfile("asdf")
+# with_op = curry(bring(1, _wlth))
+#
+# thing = wlth(open("asdfa"), lambda f: f.read())
+# thing = wlth(open("asdfa"))(lambda f: f.read())
+#
+# from typing import Union
+# from typedtoolz.functoolz import excepts, union_error
+# A1 = TypeVar("A1")
+# A2 = TypeVar("A2")
+# A3 = TypeVar("A3")
+#
+# file_errors = (
+#     FileNotFoundError,
+#     PermissionError,
+#     IsADirectoryError,
+#     NotADirectoryError,
+#     FileExistsError,
+#     ValueError,
+#     TypeError,
+#     UnsupportedOperation,
+#     LookupError,
+# )
+# open_errors = (
+#     ValueError,
+#     TypeError,
+#     UnsupportedOperation,
+#     BlockingIOError,
+#     InterruptedError,
+#     TimeoutError,
+#     IsADirectoryError,
+#     NotADirectoryError,
+#     PermissionError,
+#     FileNotFoundError,
+#     ConnectionResetError,
+#     ConnectionAbortedError,
+#     BrokenPipeError,
+# )
+#
+# saferead = union_error(
+#         open_errors, 
+#         with_op(
+#             cast(
+#                 Callable[[TextIOBase], str],
+#                 lambda f: f.read()
+#                 )
+#             )
+#         )
+# res = saferead(open("asdf"))
+# safereadfile = union_error(file_errors, lambda path: saferead(path))
+# res = safereadfile("asdf")
