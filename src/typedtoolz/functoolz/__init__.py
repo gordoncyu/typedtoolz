11a8d08b0a35fa5164ac258d9fc3afda75c0a905
gordoncyu <gordoncyu@gmail.com>
2026-02-28 14:59:31 -0700

    cleaned up reduce

--- src/typedtoolz/functoolz/__init__.py
+++ src/typedtoolz/functoolz/__init__.py
@@ -1,7 +1,7 @@
 from .exceptions import excepts, union_error
 from ._finalizer import finalizer
 from ._with import wlth
-from ._reduce import reduce, reduced
+from ._reduce import reduce
 from . import _return_parameter as return_parameter
 from ._pipe import pipe
 from ._flow import flow
@@ -13,7 +13,6 @@ __all__ = [
         "union_error",
         "finalizer",
         "reduce",
-        "reduced",
         "wlth",
         "return_parameter",
         "pipe",
--- src/typedtoolz/functoolz/_reduce.py
+++ src/typedtoolz/functoolz/_reduce.py
@@ -1,37 +1,86 @@
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, cast, get_args
 from collections.abc import Iterable
+from typing_extensions import overload, override
 from typedtoolz.functoolz._curry import curry
 
 A = TypeVar("A")
 R = TypeVar("R")
 
-_initial_missing = object() # instead of a propper bottom value, a unique object
-# is used because people use the bottom value "None" too much to mean other things
-# this is straight from the stdlib bro, man python makes you do some wild stuff
+_initial_missing = object()
 
 # stolen from stdlib bcuz passing initial=foo would break if using the builtin
 # for some reason the builtin behavior differs from the src behavior because dumb
-# all so I can use pipe more ergonomically. Gosh if I had to wrap everything in 
-# lambdas everywhere that would be pain.
-def _reduce(
-    function: Callable[[A, A], A],
-    sequence: Iterable[A],
-) -> A:
-    it = iter(sequence)
-    try:
-        value = next(it)
-    except StopIteration:
-        raise TypeError("reduce() of empty iterable with no initial value") from None
-    return _reduced(function, value, it)
+# all so I can use pipe more ergonomically.
+class _reduce_meta(type):
+    @classmethod
+    @overload
+    def __call__(
+            cls,
+            function: Callable[[A, A], A],
+            sequence: Iterable[A], 
+            ) -> A: ...
+    @classmethod
+    @overload
+    def __call__(
+            cls,
+            function: Callable[[R, A], R],
+            sequence: Iterable[A], 
+            initial: R = cast(R, _initial_missing),  # pyright: ignore[reportCallInDefaultInitializer]
+            ) -> R: ...
 
-def _reduced(function: Callable[[R, A], R], initial: R, sequence: Iterable[A]) -> R:
-    it = iter(sequence)
 
-    value = initial
-    for element in it:
-        value = function(value, element)
+    @staticmethod
+    def _reduce(
+            function: Callable[[A | R, A], R],
+            sequence: Iterable[A], 
+            initial: R,
+            ) -> R:
+        it = iter(sequence)
 
-    return value
+        if initial is _initial_missing:
+            try:
+                value = next(it)
+            except StopIteration:
+                raise TypeError("reduce() of empty sequence with no initial value") from None
+        else:
+            value = initial
 
-reduce = curry(_reduce)
-reduced = curry(_reduced)
+        for element in it:
+            value = function(value, element)
+
+        return value  # pyright: ignore[reportReturnType]
+
+    @classmethod
+    @override
+    def __call__(  # pyright: ignore[reportInconsistentOverload]
+             cls,
+            function: Callable[[A | R, A], R],
+            sequence: Iterable[A], 
+            initial: R = cast(R, _initial_missing),  # pyright: ignore[reportCallInDefaultInitializer]
+            ) -> R:
+        return cls._reduce(function, sequence, initial)
+
+class reduce(metaclass=_reduce_meta):
+    """
+    reduce(function, iterable[, initial], /) -> value
+
+    Apply a function of two arguments cumulatively to the items of an iterable, from left to right.
+
+    This effectively reduces the iterable to a single value.  If initial is present,
+    it is placed before the items of the iterable in the calculation, and serves as
+    a default when the iterable is empty.
+
+    For example, reduce(lambda x, y: x+y, [1, 2, 3, 4, 5])
+    calculates ((((1 + 2) + 3) + 4) + 5).
+
+    Has curried versions as properties prefixed with c
+    """
+    c = curry(2, _reduce_meta.__call__)  # pyright: ignore[reportUnannotatedClassAttribute]
+    ci = curry(_reduce_meta._reduce)  # pyright: ignore[reportUnannotatedClassAttribute, reportPrivateUsage]
+
+thing = reduce(
+        lambda acc, v: acc + v,
+        [1],
+        2
+        )
+
