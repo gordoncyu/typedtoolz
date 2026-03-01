6cc571acb15447a421a36900d6beeda12b9d084c
gordoncyu <gordoncyu@gmail.com>
2026-02-28 22:12:48 -0700

    improved map

--- src/typedtoolz/itertoolz/_map.py
+++ src/typedtoolz/itertoolz/_map.py
@@ -1,6 +1,50 @@
-from typedtoolz.functoolz import curry
+from collections.abc import Iterable, Iterator
+from typing import Callable, TypeVar
+from typing_extensions import Any, overload, override
+from typedtoolz.functoolz import curry, curryv
+from builtins import map as rmap
+import sys
 
-map = curry(map)
+_T1 = TypeVar("_T1")
+_T2 = TypeVar("_T2")
+_T3 = TypeVar("_T3")
+_T4 = TypeVar("_T4")
+_T5 = TypeVar("_T5")
+_S = TypeVar("_S")
+class _map_meta(type):
+    if sys.version_info >= (3, 14): # 3.14 adds `strict` argument.
+        @staticmethod
+        @override
+        def __call__(
+                func: Callable[..., _S],
+                iterable: Iterable[Any],  # pyright: ignore[reportExplicitAny]
+                /,
+                *iterables: Iterable[Any],  # pyright: ignore[reportExplicitAny]
+                strict: bool = False,
+                ) -> Iterator[_S]:
+            return rmap(func, iterable, *iterables, strict=strict)  # pyright: ignore[reportCallIssue]
+    else:
+        @staticmethod
+        @override
+        def __call__(
+                func: Callable[..., _S],
+                iterable: Iterable[Any],  # pyright: ignore[reportExplicitAny]
+                /,
+                *iterables: Iterable[Any],  # pyright: ignore[reportExplicitAny]
+                ) -> Iterator[_S]:
+            return rmap(func, iterable, *iterables)
+
+class map(metaclass=_map_meta):
+    """
+    Make an iterator that computes the function using arguments from
+    each of the iterables.  Stops when the shortest iterable is exhausted.
+
+    If strict is true and one of the arguments is exhausted before the others,
+    raise a ValueError.
+
+    Has a curried version as the property c
+    """
+    c = curryv(2, _map_meta.__call__)  # pyright: ignore[reportUnannotatedClassAttribute]
 
 __all__ = [
         "map",
--- tests/test_map.py
+++ tests/test_map.py
@@ -4,12 +4,12 @@ from typedtoolz.itertoolz import map
 def test_map_doubles():
     def double(x: int) -> int:
         return x * 2
-    doublem = map(double)
+    doublem = map.c(double)
     res = doublem([1, 2, 3])
     res = list(res)
     assert res == [2, 4, 6]
 
 def test_map_upper():
-    _ = map(str.upper)
+    _ = map.c(str.upper)
     _ = _(["a", "b"])
     assert list(_) == ["A", "B"]
