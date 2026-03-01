44408608bb8643f93fabe21f578e3c34e95b7378
gordoncyu <gordoncyu@gmail.com>
2026-02-28 21:30:16 -0700

    made takewhile_acc better

--- src/typedtoolz/itertoolz/_takewhile_acc.py
+++ src/typedtoolz/itertoolz/_takewhile_acc.py
@@ -1,29 +1,87 @@
 from collections.abc import Callable, Iterable
-from typing import TypeVar
-from typedtoolz.functoolz import curryv
+from typing import TYPE_CHECKING, Generic, ReadOnly, TypeVar, cast, reveal_type
+from typing_extensions import TypedDict, overload, override
+from typedtoolz.functoolz._curryv import curryv
+from typedtoolz.functoolz._reduce import _initial_missing  # pyright: ignore[reportPrivateUsage]
 
 A = TypeVar('A')
 T = TypeVar('T')
 
-def _takewhile_acc(
-    func: Callable[[A, T], tuple[bool, A]],
-    initial_acc: A,
-    iterable: Iterable[T],
-    take_first_negative: bool = False,
-) -> list[T]:
-    result: list[T] = []
-    acc = initial_acc
+class TakeAcc(Generic[A], TypedDict, extra_items=A):
+    take: bool
 
-    for item in iterable:
-        predicate, acc = func(acc, item)
-        if not predicate:
-            if take_first_negative: result.append(item)
-            break
-        result.append(item)
+class _takewhile_acc_meta(type):
+    @classmethod
+    @overload
+    def __call__(
+            cls,
+            func: Callable[[A, T], tuple[bool, A] | TakeAcc[A]],
+            iterable: Iterable[T],
+            *,
+            take_first_negative: bool = False,
+            ) -> list[T]: ...
+    @classmethod
+    @overload
+    def __call__(
+            cls,
+            func: Callable[[A, T], tuple[bool, A] | TakeAcc[A]],
+            iterable: Iterable[T],
+            initial: A = cast(A, _initial_missing),  # pyright: ignore[reportCallInDefaultInitializer]
+            *,
+            take_first_negative: bool = False,
+            ) -> list[T]: ...
 
-    return result
+    @classmethod
+    @override
+    def __call__(
+            cls,
+            func: Callable[[A, T], tuple[bool, A] | TakeAcc[A]],
+            iterable: Iterable[T],
+            initial: A = cast(A, _initial_missing),  # pyright: ignore[reportCallInDefaultInitializer]
+            *,
+            take_first_negative: bool = False,
+            ) -> list[T]:
+        return cls._call(func, initial, iterable, take_first_negative)
 
-takewhile_acc = curryv(3, _takewhile_acc)
+    @staticmethod
+    def _call(
+        func: Callable[[A, T], tuple[bool, A] | TakeAcc[A]],
+        initial: A,
+        iterable: Iterable[T],
+        take_first_negative: bool = False,
+    ) -> list[T]:
+        it = iter(iterable)
+
+        if initial is _initial_missing:
+            try:
+                acc = next(it)
+            except StopIteration:
+                raise TypeError("reduce() of empty sequence with no initial value") from None
+        else:
+            acc = initial
+
+        result: list[T] = []
+        acc = initial
+
+        for item in it:
+            match func(acc, item):
+                case (predicate, acc): ...
+                case {"take": predicate, **rest}: 
+                    try:
+                        acc = cast(A, next(iter(rest.values())))
+                    except StopIteration:
+                        raise ValueError(f"Provided accumulation function returned a dictionary with no extra key to treat as the accumulator")
+                case bad_value: raise ValueError(f'Provided accumulation function must return either (bool, T) or {{"take": bool, [any string]: T}}, instead got: {bad_value}') # This never happens, pyright knows takeacc type has been covered yet it does not care, hence i need this case to be exhaustive according to it
+            if not predicate:
+                if take_first_negative: result.append(item)
+                break
+            result.append(item)
+
+        return result
+
+class takewhile_acc(metaclass=_takewhile_acc_meta):
+    c = curryv(2, _takewhile_acc_meta.__call__)  # pyright: ignore[reportUnannotatedClassAttribute]
+    ci = curryv(3, _takewhile_acc_meta._call)  # pyright: ignore[reportUnannotatedClassAttribute, reportPrivateUsage]
 
 __all__ = [
         "takewhile_acc",
