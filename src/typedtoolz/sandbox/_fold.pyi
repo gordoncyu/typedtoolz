a7320ca71d0aa304089ac0761ec8294d98bd66d3
gordoncyu <gordoncyu@gmail.com>
2026-03-02 22:07:58 -0700

    improved fold map typing

--- src/typedtoolz/sandbox/_fold.pyi
+++ src/typedtoolz/sandbox/_fold.pyi
@@ -1,16 +1,17 @@
-# TODO: Improve map typing
 from collections.abc import Callable, Iterable
 from typing import TypeVar, overload
 
 T = TypeVar('T')
 A = TypeVar('A')
+B = TypeVar('B')
+C = TypeVar('C')
 
 @overload
 def fold(
     binop: Callable[[T, T], T],
     seq: Iterable[T],
     *,
-    map: Callable[..., object] = ...,
+    map: Callable[[Callable[[B], C], Iterable[B]], Iterable[C]] = ...,
     chunksize: int = ...,
     combine: Callable[[T, T], T] | None = ...,
 ) -> T: ...
@@ -19,7 +20,7 @@ def fold(
     binop: Callable[[A, T], A],
     seq: Iterable[T],
     default: A,
-    map: Callable[..., object] = ...,
+    map: Callable[[Callable[[B], C], Iterable[B]], Iterable[C]] = ...,
     chunksize: int = ...,
     combine: Callable[[A, A], A] | None = ...,
 ) -> A: ...
